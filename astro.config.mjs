4cade8d7f0ae177893a5facda04e11198acacdb4
青稚 <130018882+Linux-qitong@users.noreply.github.com>
2024-12-28 17:05:19 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://linux-qitong.github.io/",
-  base: "/new-blog",
+  site: "https://newblog.linux-qitong.top",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(
